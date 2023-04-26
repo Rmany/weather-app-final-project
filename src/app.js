64474539dd0e70f2faf0e0c9670f8f006b7475ab
Rmany <128521849+Rmany@users.noreply.github.com>
2023-04-26 23:06:23 +0100

    Added time and date update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,50 @@
+function formatTime(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[time.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatDate(timestamp) {
+  let time = new Date(timestamp);
+
+  let date = time.getDate();
+
+  let year = time.getFullYear();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[time.getMonth()];
+  return `${date} ${month} ${year}`;
+}
+
 function updateData(input) {
   console.log(input.data);
   let temperature = document.querySelector("#mainTemp");
@@ -10,11 +57,15 @@ function updateData(input) {
   windSpeed.innerHTML = Math.round(input.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = input.data.temperature.humidity;
+  let time = document.querySelector("#time");
+  time.innerHTML = formatTime(input.data.time * 1000);
+  let date = document.querySelector("#date");
+  date.innerHTML = formatDate(input.data.time * 1000);
 }
 
 let apiKey = "9ae090e1584act3b4ed90adf0ce9o7fa";
 let units = "metric";
-let city = "cairo";
+let city = "lusaka";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(updateData);
