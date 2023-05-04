523e6ba172aa2746dffa3d4c5080fbb783e57d14
Rmany <128521849+Rmany@users.noreply.github.com>
2023-05-04 21:43:28 +0100

    Cleaned up JS

--- src/app.js
+++ src/app.js
@@ -51,61 +51,7 @@ function findDay(timestamp) {
   days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
-
-function displayForecast(input) {
-  let forecast = input.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastData = `<div class="row">`;
-  forecast.forEach((day, index) => {
-    if (index < 6) {
-      celsiusForecastHigh = day.temperature.maximum;
-      celsiusForecastLow = day.temperature.minimum;
-      forecastData =
-        forecastData +
-        `<div class="col-2">
-              ${findDay(day.time)}
-              <img
-                src="${day.condition.icon_url}"
-                alt=""
-                width="60px"
-              />
-              <div class="daily-high">${Math.round(celsiusForecastHigh)}°C</div
-              ><div class="daily-low"> ${Math.round(celsiusForecastLow)}°C</div>
-            </div>`;
-    }
-  });
-  forecastElement.innerHTML = forecastData + `</div>`;
-}
-
-function getForecast(location) {
-  let city = location;
-  let apiKey = "9ae090e1584act3b4ed90adf0ce9o7fa";
-  let units = "metric";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function updateData(input) {
-  let temperature = document.querySelector("#mainTemp");
-  celsiusTemp = input.data.temperature.current;
-  temperature.innerHTML = Math.round(celsiusTemp);
-  let location = document.querySelector("#location");
-  location.innerHTML = input.data.city;
-  let realFeel = document.querySelector("#realFeel");
-  celsiusRealFeel = input.data.temperature.feels_like;
-  realFeel.innerHTML = `${Math.round(celsiusRealFeel)}°C`;
-  let windSpeed = document.querySelector("#windSpeed");
-  windSpeed.innerHTML = Math.round(input.data.wind.speed);
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = input.data.temperature.humidity;
-  let description = document.querySelector("#description");
-  description.innerHTML = input.data.condition.description;
-  let time = document.querySelector("#time");
-  time.innerHTML = formatTime(input.data.time * 1000);
-  let date = document.querySelector("#date");
-  date.innerHTML = formatDate(input.data.time * 1000);
-  let icon = document.querySelector("#weatherIcon");
+function updateStyling() {
   let background = document.querySelector("#background");
   let searchButton = document.querySelector("#search-button");
   let locationButton = document.querySelector("#location-button");
@@ -137,8 +83,75 @@ function updateData(input) {
     locationButton.classList.add("btn");
     locationButton.classList.add("btn-success");
   }
+}
+
+function displayForecast(input) {
+  let forecast = input.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastData = `<div class="row">`;
+  forecast.forEach((day, index) => {
+    if (index < 6) {
+      celsiusForecastHigh = day.temperature.maximum;
+      celsiusForecastLow = day.temperature.minimum;
+      forecastData =
+        forecastData +
+        `<div class="col-2">
+              ${findDay(day.time)}
+              <img
+                src="${day.condition.icon_url}"
+                alt=""
+                width="60px"
+              />
+              <div class="daily-high">${Math.round(celsiusForecastHigh)}°C</div
+              ><div class="daily-low"> ${Math.round(celsiusForecastLow)}°C</div>
+            </div>`;
+    }
+  });
+  forecastElement.innerHTML = forecastData + `</div>`;
+  updateStyling();
+}
+
+function getForecast(location) {
+  let city = location;
+  let apiKey = "9ae090e1584act3b4ed90adf0ce9o7fa";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function updateData(input) {
+  console.log(input);
+  let temperature = document.querySelector("#mainTemp");
+  let location = document.querySelector("#location");
+  let realFeel = document.querySelector("#realFeel");
+  let windSpeed = document.querySelector("#windSpeed");
+  let humidity = document.querySelector("#humidity");
+  let description = document.querySelector("#description");
+  let time = document.querySelector("#time");
+  let date = document.querySelector("#date");
+  let icon = document.querySelector("#weatherIcon");
+
+  celsiusTemp = input.data.temperature.current;
+  temperature.innerHTML = Math.round(celsiusTemp);
+
+  location.innerHTML = input.data.city;
+
+  celsiusRealFeel = input.data.temperature.feels_like;
+  realFeel.innerHTML = `${Math.round(celsiusRealFeel)}°C`;
+
+  windSpeed.innerHTML = Math.round(input.data.wind.speed);
+
+  humidity.innerHTML = input.data.temperature.humidity;
+
+  description.innerHTML = input.data.condition.description;
+
+  time.innerHTML = formatTime(input.data.time * 1000);
+
+  date.innerHTML = formatDate(input.data.time * 1000);
 
   icon.setAttribute("src", input.data.condition.icon_url);
+
   getForecast(input.data.city);
 }
 
